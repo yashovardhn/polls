afa524c9b4a4924534497f31db917fbed1bd9032
Yashovardhn <yashovardhn@gmail.com>
2024-06-03 17:16:00 +0530

    Initial commit

---
+++ django-polls/polls/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Choice, Question
+from django.utils import timezone
+import datetime
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self, obj):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= obj.pub_date <= now
+
+
+admin.site.register(Question, QuestionAdmin)
---
+++ django-polls/polls/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class DjangoPollsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_polls'
+    label = 'django_polls'
