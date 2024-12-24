835b87ed28f425ff4fe1a7ad87ed54fa809c4d4f
Yashovardhn <yashovardhn@gmail.com>
2024-12-24 12:00:15 +0530

    ok

--- django-polls/polls/tests.py
+++ django-polls/polls/tests.py
@@ -14,7 +14,6 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
-
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
